52e84f0ab1110645711d2fa529e4d9449abb0847
Michael Youssef <michael.kamel.2020@gmail.com>
2020-03-31 13:01:13 +0200

    add piplock

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="DisplayManagement",
+    name="displaymanagement",
     version="0.0.1",
     description="A library for managing the Displays connected to EvoCount's PDM",
     url="https://gitlab.com/evocount/display-management",
-    author="EvoCount",
+    author="EvoCount GmbH",
     packages=["displaymanagement", "displaymanagement/model_descriptors"],
-    python_requires=">=3.0",
+    python_requires=">=3.6",
     install_requires=["pydantic", "python-xlib", "pyedid"],
 )
