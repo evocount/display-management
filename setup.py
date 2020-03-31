8826436363d7333e14179e70b11855f602b3bb48
Michael Youssef <michael.kamel.2020@gmail.com>
2020-03-31 13:05:16 +0200

    change url to github

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="displaymanagement",
     version="0.0.1",
     description="A library for managing the Displays connected to EvoCount's PDM",
-    url="https://gitlab.com/evocount/display-management",
+    url="https://github.com/evocount/display-management",
     author="EvoCount GmbH",
     packages=["displaymanagement", "displaymanagement/model_descriptors"],
     python_requires=">=3.6",
